114b10f2ed9345a94dc9d504a1f981d565d15a7a
V1PERFX <rafael_fiuza@ymail.com>
2021-01-27 22:54:39 -0300

    :hammer: Add input

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
@@ -27,6 +28,9 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
@@ -36,10 +40,25 @@ export default function Home() {
         <QuizLogo src={db.logo} alt="Logo Genshin Impact" />
         <Widget>
           <Widget.Header>
-            <h1>Gesnhin Impact Quiz!</h1>
+            <h1>Quiz da Paimon!</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem impusm dolor sit amet...</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Qual seu nome viajante?"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
